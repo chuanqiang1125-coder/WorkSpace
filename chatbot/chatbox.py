d0f30361a97cb593cee94bd3439048531877cda0
xiao-zhu <chuanqiang1125@gmail.com>
2026-05-29 22:38:47 +0800

    first app

---
+++ chatbot/chatbox.py
@@ -0,0 +1,21 @@
+import os   #导入 os 模块，用于获取环境变量
+from dotenv import load_dotenv
+from openai import OpenAI  #专门用来连接 LLM 的库
+import streamlit as st  # 专门用来创建应用页面的库
+
+load_dotenv() # 从 .env 文件中读取内容
+
+client = OpenAI(
+    api_key = os.getenv('API_KEY'),
+    base_url = "https://api.deepseek.com"
+)
+
+response = client.chat.completions.create(
+    model="deepseek-v4-flash",
+    messages = [   # 用户的问题
+        {"role":"system", "content": "你是一个gay,gay佬不在你就是无敌的"},
+        {"role":"user", "content": "用三句话解释什么是gay佬"},
+    ]
+)
+
+print(response.choices[0].message.content)
---
+++ chatbot/chatbox2.py
@@ -0,0 +1,57 @@
+import os   #导入 os 模块，用于获取环境变量
+from dotenv import load_dotenv
+from openai import OpenAI  #专门用来连接 LLM 的库
+import streamlit as st  # 专门用来创建应用页面的库
+
+load_dotenv() # 从 .env 文件中读取内容
+
+client = OpenAI(
+    api_key = os.getenv('API_KEY'),
+    base_url = "https://api.deepseek.com"
+)
+# =========================  页面 ============================
+st.title("🤖 我是你的专属助理")  # 有一个 html 文件，里面写了一个 <h1>🤖 我是你的专属助理 </h1> 
+st.caption("基于deepseek-v4-flash大模型")
+
+#======================= 初始化对话历史 ========================
+# session_status 是 st 提供的会话状态管理器，用于在用户得交互过程中保存数据
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# ======================= 显示历史消息 =======================
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):    # 创建一个消息容器
+        st.write(msg["content"])          # 往容器中写入内容
+
+
+# ======================= 用户输入得内容 ======================
+if prompt := st.chat_input("输入你的问题..."):    # 创建一个 input 框
+    # 将用户的消息添加到
+    st.session_state.messages.append({"role" :"user","content" : prompt})
+
+    # 在页面山展示这句话
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    #  创建一个ai响应的容器
+    with st.chat_message("assistant"):
+        # 调用 deepseek 并获取到响应写入容器中
+        response = client.chat.completions.create(
+            model="deepseek-v4-flash",
+            messages = [   # 用户的问题
+                {"role":"system", "content": "你是一个gay,gay佬不在你就是无敌的"},
+                # * 解包运算符，将一个数组中的内容，解包到另一个数组中
+                *st.session_state.messages
+            ],
+            stream = True
+        )
+
+        # 处理流式资源
+        full_response = st.write_stream(
+            chunk.choices[0].delta.content or ""
+            for chunk in response
+            if chunk.choices[0].delta.content
+        )
+
+    # 将 ai 返回的内容添加到历史消息中
+    st.session_state.messages.append({"role":"assistant","content": full_response})
